a626f69eb79dec66fcd93875c9753c851b515aae
choikang <choikang7157@naver.com>
2025-09-10 09:41:56 +0900

    git init

---
+++ src/main/java/com/example/capstone_java/CapstoneJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.capstone_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapstoneJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/capstone_java/CapstoneJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.capstone_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
