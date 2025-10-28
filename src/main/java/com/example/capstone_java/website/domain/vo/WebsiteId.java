c25d6d96bc4ab49fcd04560f08a5568f4f36b1e7
choikang <choikang7157@naver.com>
2025-10-28 15:04:55 +0900

    Refactor(#3): 도메인 엔티티 리팩토링 - 이벤트 생성 책임 분리

--- src/main/java/com/example/capstone_java/website/domain/vo/WebsiteId.java
+++ src/main/java/com/example/capstone_java/website/domain/vo/WebsiteId.java
@@ -1,14 +1,27 @@
 package com.example.capstone_java.website.domain.vo;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+
+import java.util.UUID;
 
 @EqualsAndHashCode
 @Getter
 public final class WebsiteId {
-    private Long id;
+    private final UUID id;
 
-    public WebsiteId(final Long id) {
+    @JsonCreator
+    public WebsiteId(@JsonProperty("id") final UUID id) {
         this.id = id;
+    }
+
+    public static WebsiteId of(UUID id) {
+        return new WebsiteId(id);
+    }
+
+    public static WebsiteId generate() {
+        return new WebsiteId(UUID.randomUUID());
     }
 }
