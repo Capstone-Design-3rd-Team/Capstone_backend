d9d22f26bebb98a256e217c33c9639e560000eef
choikang <choikang7157@naver.com>
2025-10-28 15:05:25 +0900

    Refactor(#3): 애플리케이션 서비스 리팩토링 - 크롤링 제한 정책 버그 수정

--- src/main/java/com/example/capstone_java/website/application/port/in/usecase/CrawlUrlsUseCase.java
+++
@@ -1,7 +0,0 @@
-package com.example.capstone_java.website.application.port.in.usecase;
-
-import com.example.capstone_java.website.domain.vo.WebsiteId;
-
-public interface CrawlUrlsUseCase {
-    void startCrawling(WebsiteId websiteId, String mainUrl);
-}
